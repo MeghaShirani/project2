98b92dd946b49bb114ea6764113e28bfc9e1887f
Ubuntu <ubuntu@ip-172-31-18-69.ec2.internal>
2022-06-21 15:54:47 +0000

    sending

---
+++ fact.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+num="5 6 7 4 8 12"
+read num
+for i in $num
+do 
+	fact=1
+	while [ $i -gt 1 ]
+	do
+		fact =`expr $num \* $fact'
+		n='expr $num - 1`
+	done
+	echo "the factorial is $fact"
+done
+
---
+++ strrev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "enter the string"
+read str
+reverse=`echo $str | rev`
+echo "the result is $reverse"
+
